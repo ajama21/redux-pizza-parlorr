d10ec8b67e7b5fa1af59ce7aed854389dd8f78f8
Najma Jama <Najma55.nj@gmail.com>
2023-09-21 14:53:54 -0500

    pizza displaying on dom, pizza list, added remove button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import PizzaListItem from "../PizzaListItem/PizzaListItem";
+import PizzaList from "../PizzaList/PizzaList";
 
 function App() {
+  const dispatch = useDispatch();
+  const pizzaList = useSelector((store) => store.pizzaReducer);
+
+  // get Pizza data from server on load
+  useEffect(() => {
+    console.log("in useEffect");
+    fetchPizza();
+  }, []);
+
+  const fetchPizza = () => {
+    axios({
+      method: "GET",
+      url: "/api/pizza",
+    })
+      .then((response) => {
+        // response.data is the array of artists
+        console.log(response.data);
+        // TODO - update this to dispatch to Redux
+        dispatch({
+          type: "GET_PIZZA",
+          payload: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log("error on GET", error);
+      });
+  };
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Prime Pizza</h1>
       </header>
-  
-      <img src='images/pizza_photo.png' />
+
+      <img src="images/pizza_photo.png" />
       <p>Pizza is great.</p>
-  
+      <PizzaList fetchPizza={fetchPizza} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+
+const pizzaReducer = (state = [], action) => {
+    if(action.type === 'GET_PIZZA'){
+      return action.payload;
+    }
+    return state;
+  }
+
+  const storeInstance = createStore(
+    combineReducers({
+      pizzaReducer,
+    }),
+    applyMiddleware(logger),
+  );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+    <Provider store={storeInstance}>
         <App />
+    </Provider>
     </React.StrictMode>
 );
+
